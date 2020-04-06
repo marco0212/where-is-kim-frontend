5b580bb0f2449b3c401881b43d90c205d2ffe2be
Jeong <inyeop0212@gmail.com>
2020-04-06 21:22:51 +0900

    Fix user reducer

--- src/reducer/user.js
+++ src/reducer/user.js
@@ -1,33 +1,35 @@
 import { UPDATE_USER } from "../constants";
 
 const initialState = {
-  username: '',
-  email: '',
+  id: "",
+  username: "",
+  email: "",
   allTeams: [],
-  teamById: {}
+  teamById: {},
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case UPDATE_USER:
-      const { username, email, teams } = action.payload;
-      const allTeams = teams.map(team => team.id);
+      const { _id, username, email, teams } = action.payload;
+      const allTeams = teams.map((team) => team._id);
       const teamById = teams.reduce((acc, team) => {
-        acc[team.id] = team;
+        acc[team._id] = team;
         return acc;
       }, {});
 
       return {
         ...state,
+        id: _id,
         username,
         email,
         allTeams,
-        teamById
+        teamById,
       };
-      
+
     default:
       return {
-        ...state
+        ...state,
       };
   }
 }
