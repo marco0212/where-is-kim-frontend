40a45adf12bc4002030c6ad3a03c6b5f14f9192b
Jeong <inyeop0212@gmail.com>
2020-04-17 21:23:42 +0900

    Fix router as browser router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Routes from "../../routes";
 import GlobalStyle from "../../styles/GlobalStyle";
 
 export default function App({ isLogin }) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle />
       <Routes isLogin={isLogin} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
