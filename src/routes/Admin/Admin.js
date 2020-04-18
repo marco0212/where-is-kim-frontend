ea35906fd54f6719129a6cc000d927b9bf06a739
Jeong <inyeop0212@gmail.com>
2020-04-18 12:51:04 +0900

    Fix admin route

--- src/routes/Admin/Admin.js
+++ src/routes/Admin/Admin.js
@@ -1,23 +1,41 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Dashboard from "../Dashboard/Container";
 import Analytics from "../Analytics/Container";
 import Participants from "../Participants/Container";
 import styled from "styled-components";
 
-export default function Admin() {
+const Verified = () => {
   return (
-    <Wrapper>
-      <Switch>
-        <Route exact path="/team/:name/admin/" component={Dashboard} />
-        <Route path="/team/:name/admin/analytics" component={Analytics} />
-        <Route path="/team/:name/admin/participants" component={Participants} />
-      </Switch>
-    </Wrapper>
+    <Switch>
+      <Route exact path="/team/:name/admin/" component={Dashboard} />
+      <Route path="/team/:name/admin/analytics" component={Analytics} />
+      <Route path="/team/:name/admin/participants" component={Participants} />
+    </Switch>
   );
+};
+
+const Unverified = () => {
+  return <Button to="/">허튼 짓하지 말아요..</Button>;
+};
+
+export default function Admin({ isAdmin }) {
+  return <Wrapper>{isAdmin ? <Verified /> : <Unverified />}</Wrapper>;
 }
 
 const Wrapper = styled.div`
   min-height: 100%;
   padding: 30px 30px 0;
+`;
+const Button = styled(Link)`
+  display: inline-block;
+  padding: 0 15px;
+  background-color: #666;
+  border: 0;
+  vertical-align: top;
+  line-height: 35px;
+  color: #fff;
+  border-radius: 4px;
+  text-align: center;
+  text-transform: uppercase;
 `;
--- src/routes/Admin/Container.js
+++ src/routes/Admin/Container.js
@@ -2,11 +2,13 @@ import React from "react";
 import Admin from "./Admin";
 import { connect } from "react-redux";
 
-function AdminContainer() {
-  return <Admin />;
+function AdminContainer({ isAdmin }) {
+  return <Admin isAdmin={isAdmin} />;
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  isAdmin: state.ui.admin.isAdmin,
+});
 const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminContainer);
